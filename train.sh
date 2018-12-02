d6c6eaef6f78be272cb0a705ff4db7a9eaf81b7a
Corentin Dancette <corentin.dancette@gmail.com>
2018-12-02 20:47:59 +0000

    train

--- train.sh
+++ train.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 date=`date '+%Y-%m-%d-%Hh%M.%S'`
+mkdir -p experiments/$date/
+
+cp train.sh experiments/$date
+
+logs=experiments/$date/logs
 
 python -u main.py --features-path data/mandarin-fbanks-training.h5f \
 	--embedding-size 30 \
 	--epochs 30 \
-	--model-path model-$date | tee log-$date.txt
+	--model-path experiments/$date/model/ \
+	--learning-rate 1e-5 \
+	| tee $logs
