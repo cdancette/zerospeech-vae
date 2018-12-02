2b0e0ece324e424206b1cfd52fbcaee44a2eda74
Corentin <corentin@cdancette.fr>
2018-12-02 15:35:00 -0500

    few changes

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py -f data/train/mandarin/mandarin-fbanks-training.h5f -s 30 | tee log.txt
+python -u main.py -f data/train/mandarin/mandarin-fbanks-training.h5f -s 30 | tee log.txt
